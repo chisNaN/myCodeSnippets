93d845c41529b59a84c5d0e65ddcded1ef39c6e0
͡° ͜ʖ ͡° <artgreg@outlook.fr>
2017-10-09 10:08:30 +0200

    Update multigithubclone.sh

--- multigithubclone.sh
+++ multigithubclone.sh
@@ -1,1 +1,5 @@
 curl "https://api.github.com/users/YOUR_USERNAME/repos?per_page=100" | grep -e 'git_url*' | cut -d \" -f 4 | xargs -L1 git clone
+
+# example with a pseudo and ssh keys
+
+curl "https://api.github.com/users/chisNaN/repos?per_page=100" | grep -e 'ssh_url*' | cut -d \" -f 4 | cut -c15- | xargs -L1 git clone gh
