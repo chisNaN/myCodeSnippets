b1421aab6cc32c90c7d61b587674db1e3306f0cd
͡° ͜ʖ ͡° <artgreg@outlook.fr>
2019-05-04 10:16:02 +0200

    Update sha256-browser.js

--- sha256-browser.js
+++ sha256-browser.js
@@ -1,4 +1,4 @@
-async function sha256(str) {
+const sha256 = async str => {
     try {
         const buf = await crypto.subtle.digest("SHA-256", new TextEncoder("utf-8").encode(str));
         return Array.prototype.map.call(new Uint8Array(buf), x => (('00' + x.toString(16)).slice(-2))).join('');
